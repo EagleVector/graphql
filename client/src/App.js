5e987d545c59c2445ea4ebf6b006cfe0ba615e55
EagleVector <kumarsubhambili3@gmail.com>
2023-08-22 19:50:49 +0530

    graphql server setup

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import { useQuery } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+const query = gql`
+  query GetTodosWithUser {
+    getTodos {
+      id
+      title
+      completed
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
+
+function App() {
+  const {data, loading} = useQuery(query);
+
+  if(loading) return <h1>Loading...</h1>;
+
+  return (
+    <div className="App">
+      <table>
+        <tbody>
+          {
+            data.getTodos.map(todo => <tr key={todo.id}>
+              <td>{todo.title}</td>
+              <td>{todo.user?.name}</td>
+              <td>{todo.user?.email}</td>
+            </tr>)
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
